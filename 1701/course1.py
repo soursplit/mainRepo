f549c3bbc2c2fc20b34f6fd5509709e5780e7f28
soursplit <otterpesce@gmail.com>
2025-09-18 18:16:57 -0600

    first commit

---
+++ 1701/course1.py
@@ -0,0 +1,121 @@
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+
+
+print((11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11)))
+
+for A in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for B in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for C in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for D in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for E in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for F in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for G in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for H in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
+A = 0
+B = 7
+C = 7
+D = 1
+E = 5
+F = 7
+G = 6
+H = 7
+I = 1
+for I in range(10):
+    answer = (11-(((A*10)+(B*9)+(C*8)+(D*7)+(E*6)+(F*5)+(G*4)+(H*3)+(I*2))%11))
+    if answer == 0:
+        print(A,"-",B,C,D,"-",E,F,G,H,I,answer)
